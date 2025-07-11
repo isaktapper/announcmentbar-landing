8b8b8cb613e37c1aef790a9da2de95f23bdbd6c8
isaktapper <isaketapper@gmail.com>
2025-07-11 21:30:09 +0200

    Update Yello.bar embed script ID in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <script src="https://app.yello.bar/embed/ntdjn5.js" defer></script>
+        <script src="https://app.yello.bar/embed/ceglrd.js" defer></script>
       </body>
     </html>
   );

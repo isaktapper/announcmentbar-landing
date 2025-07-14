e22d6ae81121292c4bcabac0321a02ed4688caba
isaktapper <isaketapper@gmail.com>
2025-07-14 09:54:04 +0200

    Revert Yello.bar embed script ID in layout to xledqb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <script src="https://app.yello.bar/embed/fckjqd.js" defer></script>
+        <script src="https://app.yello.bar/embed/xledqb.js" defer></script>
       </body>
     </html>
   );

6f781c58f6424a4f245d28d14644d8df6fe8424a
isaktapper <isaketapper@gmail.com>
2025-07-15 22:44:01 +0200

    Update embed script to use production Yello.bar source in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <script src="http://localhost:3000/embed/ceglrd.js" defer></script>
+        <script src="https://app.yello.bar/embed/ceglrd.js" defer></script>
       </body>
     </html>
   );

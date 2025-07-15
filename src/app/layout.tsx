15cb85d4459aaebe2e54c4c950d7b6f387850c95
isaktapper <isaketapper@gmail.com>
2025-07-15 20:24:46 +0200

    Update Yello.bar embed script to ba4b5s.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <script src="https://app.yello.bar/embed/nrd7s0.js" defer></script>
+        <script src="https://app.yello.bar/embed/ba4b5s.js" defer></script>
       </body>
     </html>
   );
